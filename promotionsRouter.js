352f5427a6b1991c8cf1b60f77f3125c1311db92
Maitri Coder <56553214+maitri266@users.noreply.github.com>
2021-10-01 18:26:50 +0530

    Update promotionsRouter.js

--- promotionsRouter.js
+++ promotionsRouter.js
@@ -26,27 +26,4 @@ promotionsRouter
 		res.end(`Deleting all the promotions`);
 	});
 
-promotionsRouter
-	.route('/:promotionsId')
-	.get((req, res, next) => {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(`Getting promotion #${JSON.stringify(req.params)}`);
-	})
-	.post((req, res, next) => {
-		res.statusCode = 403;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(`Forbidden Request | Cannot POST on this endpoint`);
-	})
-	.put((req, res, next) => {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(`Updated promotion #${JSON.stringify(req.params)} `);
-	})
-	.delete((req, res, next) => {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(`Deleted ${JSON.stringify(req.params)} from promotions`);
-	});
-
 module.exports = promotionsRouter;
