1da6678ec8a2fe4626d1da3fc504e05d8591f932
Maitri Gopani <56553214+maitri266@users.noreply.github.com>
2021-10-01 18:11:11 +0530

    Update dishesRouter.js
    Not needed

--- dishesRouter.js
+++ dishesRouter.js
@@ -26,27 +26,6 @@ dishesRouter
 		res.end(`Deleting all the dishes`);
 	});
 
-dishesRouter
-	.route('/:dishesId')
-	.get((req, res, next) => {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(`Getting dish #${JSON.stringify(req.params)}`);
-	})
-	.post((req, res, next) => {
-		res.statusCode = 403;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(`Forbidden Request | Cannot POST on this endpoint`);
-	})
-	.put((req, res, next) => {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(`Updated dish #${JSON.stringify(req.params)} `);
-	})
-	.delete((req, res, next) => {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(`Deleted ${JSON.stringify(req.params)} from dishes`);
-	});
+
 
 module.exports = dishesRouter;
