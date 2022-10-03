c3ed7e72e2b9e4d645a613d5f044338146f2eee2
Jash Gopani <40660648+jashgopani@users.noreply.github.com>
2022-10-03 14:37:05 +0530

    Create waiterRouter.js

---
+++ waiterRouter.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const waiterRouter = express.Router();
+waiterRouter.use(express.json());
+
+waiterRouter
+	.route('/')
+	.get((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`Getting all the waiter data`);
+	})
+	.post((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`Added New waiter to waiter list`);
+	})
+	.put((req, res, next) => {
+		res.statusCode = 403;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`You cannot update all waiter at once`);
+	})
+	.delete((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`Deleting all the waiter details`);
+	});
+
+
+
+module.exports = promotionsRouter;
